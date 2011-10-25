ce4337a584e0182ae6d3bacca0af1ddc913c0242
Luis Landeiro Ribeiro <luis.ribeiro@pdmfc.com>
2011-10-25 21:17:00 +0100

    new readme, added production configs

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ gem install sinatra
 
 # Run
     ruby -rubygems app.rb
+
+# Production
+    rackup -I . -E production -p 4567 -d
--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
-get '/' do
-  erb :index
-end
 
-markdown_pages = %w(partners people research publications contacts introduction)
+class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-markdown_pages.each do |p|
-  get "/#{p}" do
-    markdown p.to_sym, :layout_engine => :erb
+  markdown_pages = %w(partners people research publications contacts introduction)
+
+  markdown_pages.each do |p|
+    get "/#{p}" do
+      markdown p.to_sym, :layout_engine => :erb
+    end
   end
 end
