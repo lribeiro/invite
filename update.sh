9f26fc1271f00853e588fa3ec54f2f442c400477
Luis Landeiro Ribeiro <luis.ribeiro@pdmfc.com>
2011-12-01 04:44:20 +0000

    fixed update.sh call to other scripts

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+MY_DIR=`dirname $0`
 update=`git pull`
 
 #check the git pull has pulled new files, if so compile a new AI version
 if [ "$update" != "Already up-to-date." ];  
 then
   echo "reloading site"
-  shutdown.sh
-  startup.sh
+  $MY_DIR/shutdown.sh
+  $MY_DIR/startup.sh
 else
   #Exit 1 now means that the update has no new  files
   exit 1
