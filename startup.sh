71e1a63b2213c788ad22511719699014e2803f4f
istgaips <rui.prada@ist.utl.pt>
2011-12-02 19:18:49 +0000

    try to properly load rvm on startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -x
-/usr/local/rvm/bin/rvm use 1.9.2
-/usr/local/rvm/gems/ruby-1.9.2-p290/bin/rackup -s thin -I . -E production -p 4567 -D -P tmp/invite.pid
+
+# Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+
+  # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+
+else
+
+  printf "ERROR: An RVM installation was not found.\n"
+
+fi
+
+
+source rvm use 1.9.2
+rackup -s thin -I . -E production -p 4567 -D -P tmp/invite.pid
