52c8e064ab91bd7d18eed66d5760bca720de0781
istgaips <rui.prada@ist.utl.pt>
2011-12-02 19:12:04 +0000

    debug bash on startup.sh, added full path to gem

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash -x
 /usr/local/rvm/bin/rvm use 1.9.2
-rackup -s thin -I . -E production -p 4567 -D -P tmp/invite.pid
+/usr/local/rvm/gems/ruby-1.9.2-p290/bin/rackup -s thin -I . -E production -p 4567 -D -P tmp/invite.pid
