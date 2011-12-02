aef480d9c3baaae7893a1714c5a1357aab196f84
istgaips <rui.prada@ist.utl.pt>
2011-12-02 19:07:52 +0000

    removed rvm from update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,6 @@ if [ "$update" != "Already up-to-date." ];
 then
   echo "reloading site"
   $MY_DIR/shutdown.sh
-  source rvm use 1.9.2
   $MY_DIR/startup.sh
 else
   #Exit 1 now means that the update has no new  files
